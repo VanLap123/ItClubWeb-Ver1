cb04f370359e3834eea3f49a7d0fb0c65fe267ea
Lê Chí Luân <chiluan6601@gmail.com>
2022-06-14 22:18:47 +0700

    Update manageuser.php
    Phân trang Manage User. Chạy oke. Có lỗi trong phần search cần sửa lại. Bị lỗi trang nếu search.

--- manageuser.php
+++ manageuser.php
@@ -1,3 +1,33 @@
+<style>
+body {
+  background-color:white;
+
+}
+.pagination {
+
+    display: flex;
+    justify-content: center;
+    padding-top: 30px;
+    padding-bottom: 30px;
+}
+
+.pagination a {
+  color: white;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+  transition: background-color .3s;
+  border: 1px solid #ddd;
+}
+
+.pagination a.active {
+  background-color: #4CAF50;
+  color: white;
+  border: 1px solid #4CAF50;
+}
+
+.pagination a:hover:not(.active) {background-color: #ddd;}
+</style>
 <!-- body -->
 <div id="manage-container">
     <p class="head-label">Manage User</p>
@@ -16,7 +46,7 @@
                 <div class="modal-body">
                     <div class="modal-input">
                         <div class="form-group">
-                            <input type="text" name="username" id="username" class="input-info" placeholder="UserName"  style="padding-left: 10px;">
+                            <input type="text" name="username" id="username" class="input-info" placeholder="UserName" style="padding-left: 10px;">
                             <p class="error" style="margin-bottom: 0; padding-left: 10px"></p>
                         </div>
                         <div class="form-group">
@@ -32,13 +62,13 @@
                             <p class="error" style="margin-bottom: 0;"></p>
                         </div>
                         <div class="form-group">
-                             <input type="date" id="dob" name="dob" class="input-info" placeholder="DoB,...." style="padding-left: 10px;">
-                             <p class="error" style="margin-bottom: 0;"></p>
-                         </div>
-                         <div class="form-group">
-                             <input type="text" id="github" name="github" class="input-info" placeholder="Github,...." style="padding-left: 10px;">
-                             <p class="error" style="margin-bottom: 0;"></p>
-                         </div>
+                            <input type="date" id="dob" name="dob" class="input-info" placeholder="DoB,...." style="padding-left: 10px;">
+                            <p class="error" style="margin-bottom: 0;"></p>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" id="github" name="github" class="input-info" placeholder="Github,...." style="padding-left: 10px;">
+                            <p class="error" style="margin-bottom: 0;"></p>
+                        </div>
                     </div>
                     <div class="modal-sublabel">
                         <h4>Additional Info</h4>
@@ -68,16 +98,16 @@
         </div>
         <!-- finish modal user -->
 
-    
+
         <div>
             <form action="" method="POST">
-                 <div class="form-group">
-                         <input type="text" name="id" placeholder="" class="form-label">
-                  </div>
-                  <div class="button">
-                        <button type="submit" name="btn_search" id="btn_search">Search</button>
-                    </div>
-                 
+                <div class="form-group">
+                    <input type="text" name="id" placeholder="" class="form-label">
+                </div>
+                <div class="button">
+                    <button type="submit" name="btn_search" id="btn_search">Search</button>
+                </div>
+
             </form>
         </div>
         <div class="detail-right">
@@ -98,11 +128,11 @@
                         </tr>
                     </thead>
                     <tbody class="table-body">
-                    <?php 
-                         include_once('ConnectDB.php');
-                        if(isset($_POST['btn_search'])){
-                            $id= $_POST['id'];
-                            $result = mysqli_query($conn,"SELECT * FROM users WHERE StudentID like '%$id%' or username like'%$id%'");
+                        <?php
+                        include_once('ConnectDB.php');
+                        if (isset($_POST['btn_search'])) {
+                            $id = $_POST['id'];
+                            $result = mysqli_query($conn, "SELECT * FROM users WHERE StudentID like '%$id%' or username like'%$id%'");
                             while ($row = mysqli_fetch_array($result)) { ?>
                                 <tr>
                                     <td>
@@ -115,28 +145,54 @@
                                     <td> <?php echo $row['github']; ?> </td>
                                     <td> <?php echo $row['email']; ?> </td>
                                 </tr>
-                            <?php } 
-                            
+                            <?php }
+                        } else {
+                            $results_per_page = 10;
+                            // find out the number of results stored in database
+                            $sql = 'SELECT * FROM users';
+                            $result = mysqli_query($conn, $sql);
+                            $number_of_results = mysqli_num_rows($result);
+                            // determine number of total pages available
+                            (int)$number_of_pages = ceil($number_of_results / $results_per_page);
+
+                            // determine which page number visitor is currently on
+                            if (!isset($_GET['pages'])) {
+                                $page = 1;
+                            } else {
+                                $page = $_GET['pages'];
+                            }
+                            // determine the sql LIMIT starting number for the results on the displaying page
+                            $this_page_first_result = ($page - 1) * $results_per_page;
+                            $sql1 = 'SELECT * FROM users LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
+                            $result = mysqli_query($conn, $sql1);
+                            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { ?>
+                                <tr>
+                                    <td>
+                                        <a href="?page=manageuser_update&&stuid=<?php echo $row['StudentID']; ?>" class="update-name js-update-name"> <?php echo $row['StudentID']; ?> | <?php echo $row['username']; ?> </a>
+                                    </td>
+                                    <td> <?php echo $row['Department']; ?> </td>
+                                    <td> <?php echo $row['gender']; ?> </td>
+                                    <td> <?php echo $row['card_uid']; ?> </td>
+                                    <td> <?php echo $row['user_date']; ?> </td>
+                                    <td> <?php echo $row['github']; ?> </td>
+                                    <td> <?php echo $row['email']; ?> </td>
+                                </tr>
+                        <?php
+                            }
                         }
-                        else{
-                        $result = mysqli_query($conn, "SELECT * FROM users");
-                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { ?>
-                            <tr>
-                                <td>
-                                    <a href="?page=manageuser_update&&stuid=<?php echo $row['StudentID']; ?>" class="update-name js-update-name"> <?php echo $row['StudentID']; ?> | <?php echo $row['username']; ?> </a>
-                                </td>
-                                <td> <?php echo $row['Department']; ?> </td>
-                                <td> <?php echo $row['gender']; ?> </td>
-                                <td> <?php echo $row['card_uid']; ?> </td>
-                                <td> <?php echo $row['user_date']; ?> </td>
-                                <td> <?php echo $row['github']; ?> </td>
-                                <td> <?php echo $row['email']; ?> </td>
-                            </tr>
-                        <?php } 
-                        }
-                    ?>
+                        ?>
                     </tbody>
                 </table>
+                <div class="pagination">
+                </br>
+                </br>
+                <?php 
+                    // display the links to the pages
+                    for ($page = 1; $page <= $number_of_pages; $page++) {
+                        echo '<a href="admin.php?page=manageuser&&pages=' . $page . '">' . $page . '</a> ';
+                    }
+                ?>
+                </div>
             </div>
         </div>
     </div>
